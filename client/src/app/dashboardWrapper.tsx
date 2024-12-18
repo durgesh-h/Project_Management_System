8b3b735ea02d0bedf37f055bb896655c68353231
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-18 23:18:04 +0530

    Redux config  setup

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -4,16 +4,27 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 
+import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed,
+    );
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    });
 
     return (
         <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
 
             <main
-                className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg
+                className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "" : "md:pl-64"
                     }`}
             >
                 <Navbar />
@@ -25,9 +36,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
+        <StoreProvider>
 
-        <DashboardLayout>{children}</DashboardLayout>
+            <DashboardLayout>{children}</DashboardLayout>
 
+        </StoreProvider>
     );
 };
 
