6b1eb0d17452152f985051843f0f4611e2f9890a
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-18 23:36:02 +0530

    Sidebar component added

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,6 +3,7 @@
 
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 import StoreProvider, { useAppSelector } from "./redux";
 
@@ -22,7 +23,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-
+            <Sidebar />
             <main
                 className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "" : "md:pl-64"
                     }`}
